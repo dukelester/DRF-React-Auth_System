782160b021ba8eb85e4af46168af9a2f439e450a
dukelester <dukelester4@gmail.com>
2022-06-09 02:07:37 +0300

    added the components

---
+++ src/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div class="container-fluid !direction !spacing">
+        b5-row
+    </div>
+
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './containers/Login'
+import SignUp from './containers/SignUp'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Activate from './containers/Activate'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+ 
+  <Routes>
+    <Route exact path="/" element={<App />} />
+    <Route exact path="/login" element={<Login />} />
+    <Route exact path="/signup" element={<SignUp />} />
+    <Route path="/reset-password" element={<ResetPassword />} />
+    <Route path="/password-reset-confirm/:uid/token" element={<ResetPasswordConfirm />} />
+    <Route path="/activate/:uid/token" element={<Activate />} />
+  </Routes>
+  </BrowserRouter>
+   
   </React.StrictMode>
 );
 
